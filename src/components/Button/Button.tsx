5bb5edfc6f626e32d7c1f506030044c13da2f455
ShaunPx1 <shaunpritchard1@gmail.com>
2022-03-09 09:05:52 -0500

    first init

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Button.scss";
+
+export interface ButtonProps {
+  label: string;
+}
+
+const Button = (props: ButtonProps) => {
+  return <button>{props.label}</button>;
+  console.log('This package works!: ' , props.label);
+};
+
+export default Button;
